f71f93aaf78f2cdb44b0ef76165c7ceb07056b8f
Simay Gökçen <simaygokcen@firmasoft.com>
2023-08-18 23:37:29 +0300

    Initial Commit

---
+++ NetworkAPICall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkAPICall
+//
+//  Created by Simay Gökçen on 18.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
